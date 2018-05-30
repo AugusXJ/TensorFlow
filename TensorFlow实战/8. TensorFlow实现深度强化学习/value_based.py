55ebdcc19c99158e14b21e9c23522abc2ace381a
augus <541468099@qq.com>
2018-05-30 21:30:48 +0800

    add value_based RL

--- TensorFlow实战/8. TensorFlow实现深度强化学习/value_based.py
+++ TensorFlow实战/8. TensorFlow实现深度强化学习/value_based.py
@@ -1,164 +1,177 @@
 """
-13年版本DQN玩CartPole
+Tensorflow实战，强化学习，value-based
 """
 
-import gym
-import tensorflow as tf
 import numpy as np
 import random
-from collections import deque
+import itertools
+import scipy.misc
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
 
-ENV_NAME = 'CartPole-v0'
-EPISODE = 10000  # Episode limitation
-STEP = 300  # Step limitation in an episode
-TEST = 10
-
-GAMMA = 0.9  # discount factor for target Q
-INITIAL_EPSILON = 0.5  # starting value of epsilon
-FINAL_EPSILON = 0.01  # final value of epsilon
-REPLAY_SIZE = 10000  # experience replay buffer size
-BATCH_SIZE = 32  # size of minibatch
-
-
-class DQN:
-    # DQN agent
-    def __init__(self, env):
-        # init experience replay
-        self.replay_buffer = deque()
-        # init some parameters
-        self.time_step = 0
-        self.epsilon = INITIAL_EPSILON
-        self.state_dim = env.observation_space.shape[0]
-        self.action_dim = env.action_space.n
-        self.state_input = tf.placeholder("float", [None, self.state_dim])      # 输入空间
-        self.action_input = tf.placeholder("float", [None, self.action_dim])  # 动作空间
-        self.Q_value = self.create_q_network()                                  # 神经网络计算的Q值
-        self.y_input = tf.placeholder("float", [None])                          # targetQ值
-        self.optimizer = self.create_training_method()                          # 训练启动器
-
-        # Init session
-        self.session = tf.InteractiveSession()
-        self.session.run(tf.initialize_all_variables())
-
-    def create_q_network(self):
-        # network weights
-        w1 = self.weight_variable([self.state_dim, 20])
-        b1 = self.bias_variable([20])
-        w2 = self.weight_variable([20, self.action_dim])
-        b2 = self.bias_variable([self.action_dim])
-        # hidden layers
-        h_layer = tf.nn.relu(tf.matmul(self.state_input, w1) + b1)
-        # Q Value layer
-        return tf.matmul(h_layer, w2) + b2
-
-    @staticmethod
-    def weight_variable(shape):
-        initial = tf.truncated_normal(shape)
-        return tf.Variable(initial)
-
-    @staticmethod
-    def bias_variable(shape):
-        initial = tf.constant(0.01, shape=shape)
-        return tf.Variable(initial)
-
-    def create_training_method(self):
-        q_action = tf.reduce_sum(tf.multiply(self.Q_value, self.action_input), reduction_indices=1)
-        cost = tf.reduce_mean(tf.square(self.y_input - q_action))
-        return tf.train.AdamOptimizer(0.0001).minimize(cost)
-
-    def perceive(self, state, action, reward, next_state, done):
+class gameOb():
+    def __init__(self, coordinates, size, intensity, channel, reward, name):
         """
-        存储信息
-        :param state: 
-        :param action: 
-        :param reward: 
-        :param next_state: 
-        :param done: 
-        :return: 
+        物体对象类
+        :param coordinates: x，y坐标值
+        :param size: 尺寸
+        :param intensity: 亮度值 
+        :param channel: RGB颜色通道
+        :param reward: 奖励值
+        :param name: 名称
         """
-        one_hot_action = np.zeros(self.action_dim)
-        one_hot_action[action] = 1
-        self.replay_buffer.append((state, one_hot_action, reward, next_state, done))
-        if len(self.replay_buffer) > REPLAY_SIZE:
-            self.replay_buffer.popleft()
+        self.x = coordinates[0]
+        self.y = coordinates[1]
+        self.size = size
+        self.intensity = intensity
+        self.channel = channel
+        self.reward = reward
+        self.name = name
 
-        if len(self.replay_buffer) > BATCH_SIZE:
-            self.train_q_network()
+class gameEnv():
+    def __init__(self, size):
+        """
+        环境类
+        :param size: 环境尺寸   [integer. integer]
+        """
+        self.sizex = size
+        self.sizey = size
+        self.actions = 4
+        self.objects = []
+        a = self.reset()
+        plt.imshow(a, interpolation="nearest")
 
-    def train_q_network(self):
-        self.time_step += 1
-        # Step 1: obtain random minibatch from replay memory
-        minibatch = random.sample(self.replay_buffer, BATCH_SIZE)
-        state_batch = [data[0] for data in minibatch]
-        action_batch = [data[1] for data in minibatch]
-        reward_batch = [data[2] for data in minibatch]
-        next_state_batch = [data[3] for data in minibatch]
+    def reset(self):
+        """
+        环境重置
+        :return: 当前状态
+        """
+        self.objects = []
+        hero = gameOb(self.newPosition(), 1, 1, 2, None, 'hero')        # 英雄对象
+        self.objects.append(hero)
+        goal = gameOb(self.newPosition(), 1, 1, 1, 1, 'goal')           # 目标对象
+        self.objects.append(goal)
+        hole = gameOb(self.newPosition(), 1, 1, 0, -1, 'fire')          # 火 对象
+        self.objects.append(hole)
+        goal2 = gameOb(self.newPosition(), 1, 1, 1, 1, 'goal')          # 目标对象2
+        self.objects.append(goal2)
+        hole2 = gameOb(self.newPosition(), 1, 1, 0, -1, 'fire')          # 火 对象2
+        self.objects.append(hole2)
+        goal3 = gameOb(self.newPosition(), 1, 1, 1, 1, 'goal')          # 目标对象3
+        self.objects.append(goal3)
+        goal4 = gameOb(self.newPosition(), 1, 1, 1, 1, 'goal')          # 目标对象4
+        self.objects.append(goal4)
+        state = self.renderEnv()
+        self.state = state
+        return state
 
-        # Step 2: calculate y
-        y_batch = []
-        q_value_batch = self.Q_value.eval(feed_dict={self.state_input: next_state_batch})
-        for i in range(0, BATCH_SIZE):
-            done = minibatch[i][4]
-            if done:
-                y_batch.append(reward_batch[i])
+    def moveChar(self, direction):
+        """
+        移动角色
+        :param direction: 方向
+        :return: None
+        """
+        hero = self.objects[0]
+        herox = hero.x
+        heroy = hero.y
+        if direction == 0 and hero.y >= 1:
+            hero.y -= 1
+        if direction == 1 and hero.y <= self.sizey-2:
+            hero.y += 1
+        if direction == 2 and hero.x >= 1:
+            hero.x -= 1
+        if direction == 3 and hero.x <= self.sizex-2:
+            hero.x += 1
+        self.objects[0] = hero
+
+    def newPosition(self):
+        """
+        从现有空位选择一个位置
+        :return: direction  [integer, integer]
+        """
+        iterables = [range(self.sizex), range(self.sizey)]
+        points = []
+        for t in itertools.product(*iterables):     # itertools.product可以获得几个变量的所有组合
+            points.append(t)
+        current_positions = []
+        for objectA in self.objects:
+            if (objectA.x, objectA.y) not in current_positions:
+                current_positions.append((objectA.x, objectA.y))
+        for pos in current_positions:
+            points.remove(pos)
+        location = np.random.choice(range(len(points)), replace=False)
+        return points[location]
+
+    def checkGoal(self):
+        """
+        判断hero是否触碰goal or fire
+        :return: Reward, if crashed (float, boolean)
+        """
+        others = []
+        hero = None
+        for obj in self.objects:
+            if obj.name == 'hero':
+                hero = obj
             else:
-                y_batch.append(reward_batch[i] + GAMMA * np.max(q_value_batch[i]))
+                others.append(hero)
+        for other in others:
+            if hero.x == other.x and hero.y == other.y:
+                self.objects.remove(other)
+                if other.reward == 1:
+                    self.objects.append(gameOb(self.newPosition(), 1, 1, 1, 1, 'goal'))
+                else:
+                    self.objects.append(gameOb(self.newPosition(), 1, 1, 0, -1, 'fire'))
+                return other.reward, True
+        return 0.0, False
 
-        self.optimizer.run(feed_dict={
-            self.y_input: y_batch,
-            self.action_input: action_batch,
-            self.state_input: state_batch
-        })
+    def renderEnv(self):
+        """
+        刷新环境
+        :return: ndarray 
+        """
+        a = np.ones([self.sizey + 2, self.sizex + 2, 3])        # 图片size
+        a[1:-1, 1:-1, :] = 0                                    # 边框
+        for item in self.objects:
+            a[item.y + 1:item.y + item.size + 1, item.x + 1:item.x + item.size + 1, item.channel] = item.intensity
+        b = scipy.misc.imresize(a[:, :, 0], [84, 84, 1], interp='nearest')
+        c = scipy.misc.imresize(a[:, :, 1], [84, 84, 1], interp='nearest')
+        d = scipy.misc.imresize(a[:, :, 2], [84, 84, 1], interp='nearest')
+        a = np.stack([b, c, d], axis=2)
+        return a
 
-    def egreedy_action(self, state):
-        q_value = self.Q_value.eval(feed_dict={
-            self.state_input: [state]
-        })[0]
-        if random.random() <= self.epsilon:
-            return random.randint(0, self.action_dim - 1)
-        else:
-            return np.argmax(q_value)
-        # self.epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / 10000
+    def step(self, action):
+        self.moveChar(action)
+        reward, done = self.checkGoal()
+        state = self.renderEnv()
+        return state, reward, done
 
-    def action(self, state):
-        return np.argmax(self.Q_value.eval(feed_dict={
-            self.state_input: [state]
-        })[0])
+class Qnetwork():
+    def __init__(self, h_size):
+        self.scalarInput = tf.placeholder(shape=[None, 21168], dtype=tf.float32)
+        self.imageIn = tf.reshape(self.scalarInput, shape=[-1, 84, 84, 3])
+        self.conv1 = tf.contrib.layers.convolition2d(
+            inputs=self.imageIn, num_outputs=32, kernel_size=[8, 8], stride=[4, 4],
+            padding='VALID', biases_initializer=None
+        )
+        self.conv2 = tf.contrib.layers.convolition2d(
+            inputs=self.conv1, num_outputs=64, kernel_size=[4, 4], stride=[2, 2],
+            padding='VALID', biases_initializer=None
+        )
+        self.conv3 = tf.contrib.layers.convolition2d(
+            inputs=self.conv2, num_outputs=64, kernel_size=[3, 3], stride=[1, 1],
+            padding='VALID', biases_initializer=None
+        )
+        self.conv4 = tf.contrib.layers.convolition2d(
+            inputs=self.conv3, num_outputs=512, kernel_size=[7, 7], stride=[1, 1],
+            padding='VALID', biases_initializer=None
+        )
+        self.streamAC, self.streamVC = tf.split(self.conv4, 2, 3)       # 拆分成2段，维度是第三维
+        self.streamS = tf.contrib.layers.flatten(self.streamAC)
+        self.streamV = tf.contrib.layers.flatten(self.streamVC)
 
 
-def main():
-    # initialize OpenAI Gym env and dqn agent
-    env = gym.make(ENV_NAME)
-    agent = DQN(env)
 
-    for episode in range(EPISODE):
-        # initialize task
-        state = env.reset()
-        # Train
-        for step in range(STEP):
-            action = agent.egreedy_action(state)  # e-greedy action for train
-            next_state, reward, done, _ = env.step(action)
-            # Define reward for agent
-            reward_agent = -1 if done else 0.1
-            agent.perceive(state, action, reward, next_state, done)
-            state = next_state
-            if done:
-                break
-            # Test every 100 episodes
-            if episode % 100 == 0:
-                total_reward = 0
-                for i in range(TEST):
-                    state = env.reset()
-                    for j in range(STEP):
-                        env.render()
-                        action = agent.action(state)  # direct action for test
-                        state, reward, done, _ = env.step(action)
-                        total_reward += reward
-                        if done:
-                            break
-                    ave_reward = total_reward / TEST
-                print('episode: ', episode, 'Evaluation Average Reward:', ave_reward)
-                if ave_reward >= 200:
-                    break
+
 if __name__ == '__main__':
-    main()
+    env = gameEnv(size=5)
